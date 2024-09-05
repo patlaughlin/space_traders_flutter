9e70cb3470207542060073cb4e084bf259aa9042
Patrick Laughlin <prlaugh@gmail.com>
2024-09-05 15:56:07 -0700

    test register agent

--- lib/agent_repository.dart
+++ lib/agent_repository.dart
@@ -1,19 +1,29 @@
+import 'dart:convert';
+
 import 'package:http/http.dart' as http;
 
-class AgentRepository {
-  Future<Agent> registerNewAgent(String callSign, String faction) async {
-    final res = await http
-        .post(Uri.parse('https://api.spacetraders.io/v2/register'), headers: {
-      'Content-Type': 'application/json',
-    }, body: {
-      'symbol': callSign,
-      'faction': faction,
-    });
+import 'models/models.dart';
 
-    if (res.statusCode == 200) {
-      return Agent();
+class AgentRepository {
+  final http.Client client;
+
+  AgentRepository({required this.client});
+
+  Future<Agent> registerNewAgent(String symbol, String faction) async {
+    final response = await client.post(
+      Uri.parse('https://api.spacetraders.io/v2/register'),
+      body: json.encode({
+        'symbol': symbol,
+        'faction': faction,
+      }),
+      headers: {'Content-Type': 'application/json'},
+    );
+
+    if (response.statusCode == 201) {
+      final data = json.decode(response.body);
+      return Agent.fromJson(data['data']['agent']);
     } else {
-      throw Exception('Failed to register user');
+      throw Exception('Failed to register agent');
     }
   }
 // final AuthLocalDataSource localDataSource;
@@ -23,10 +33,4 @@ class AgentRepository {
 // User? getUser() {
 //   return localDataSource.getUser();
 // }
-}
-
-class Agent {
-  fromJson(Map<String, dynamic> json) {
-    return Agent();
-  }
 }
--- test/features/auth/bloc/agent_repository_test.dart
+++ test/features/auth/bloc/agent_repository_test.dart
@@ -1,48 +1,96 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:flutter_test/flutter_test.dart';
+import 'package:http/http.dart' as http;
 import 'package:mocktail/mocktail.dart';
 import 'package:space_traders/agent_repository.dart';
+import 'package:space_traders/models/models.dart';
 
-class MockAuthRepository extends Mock implements AgentRepository {}
+class MockHttpClient extends Mock implements http.Client {}
 
 void main() {
-  group('AuthRepository', () {
-    late AgentRepository authRepository;
-    late MockAuthRepository mockAuthRepository;
+  group('AgentRepository', () {
+    late AgentRepository agentRepository;
+    late Map<String, dynamic> mockResponse;
+    late MockHttpClient mockHttpClient;
+
+    setUpAll(() async {
+      final file = File('test/mocks/mock_responses/create_agent.json');
+      final jsonString = await file.readAsString();
+      mockResponse = json.decode(jsonString);
+      registerFallbackValue(
+          Uri.parse('https://api.spacetraders.io/v2/register'));
+    });
 
     setUp(() {
-      mockAuthRepository = MockAuthRepository();
-      authRepository = AgentRepository();
+      mockHttpClient = MockHttpClient();
+      agentRepository = AgentRepository(client: mockHttpClient);
     });
 
     test('registers a new agent', () async {
-      final agent = Agent();
-      when(() => mockAuthRepository.registerNewAgent(any()))
-          .thenAnswer((_) async => agent);
+      when(() => mockHttpClient.post(
+                any(),
+                body: any(named: 'body'),
+                headers: any(named: 'headers'),
+              ))
+          .thenAnswer(
+              (_) async => http.Response(json.encode(mockResponse), 201));
 
-      final result = await authRepository.registerNewAgent('user_call_sign');
-      expect(result, equals(agent));
+      final result =
+          await agentRepository.registerNewAgent('user_call_sign', 'COSMIC');
+
+      expect(result, isA<Agent>());
+      expect(
+          result.accountId, equals(mockResponse['data']['agent']['accountId']));
+      expect(result.symbol, equals(mockResponse['data']['agent']['symbol']));
+
+      verify(() => mockHttpClient.post(
+            any(),
+            body: json.encode({
+              'symbol': 'user_call_sign',
+              'faction': 'COSMIC',
+            }),
+            headers: {'Content-Type': 'application/json'},
+          )).called(1);
+    });
+
+    test('throws exception when registration fails', () async {
+      when(() => mockHttpClient.post(
+                any(),
+                body: any(named: 'body'),
+                headers: any(named: 'headers'),
+              ))
+          .thenAnswer((_) async => http.Response(
+              '{"error": {"message": "Registration failed"}}', 400));
+
+      expect(
+        () => agentRepository.registerNewAgent('user_call_sign', 'COSMIC'),
+        throwsA(isA<Exception>().having(
+            (e) => e.toString(), 'message', contains('Registration failed'))),
+      );
     });
 
     // test('retrieves a single agent');
     // test('retrieves a list of agents');
 
-    test('registering a user should store the jwt in local storage', () async {
-      final agent = Agent();
-      when(() => mockAuthRepository.registerNewAgent(any()))
-          .thenAnswer((_) async => agent);
-
-      await authRepository.registerNewAgent('user_call_sign');
-      // verify(() => mockAuthRepository.registerUser('user_call_sign'));
-    });
-
-    test('registerUser throws Exception on failure', () async {
-      when(() => mockAuthRepository.registerNewAgent(any()))
-          .thenThrow(Exception('Failed to register user'));
-
-      expect(
-        () async => await authRepository.registerNewAgent('user_call_sign'),
-        throwsA(isA<Exception>()),
-      );
-    });
+    // test('registering a user should store the jwt in local storage', () async {
+    //   final agent = Agent();
+    //   when(() => mockAuthRepository.registerNewAgent(any()))
+    //       .thenAnswer((_) async => agent);
+    //
+    //   await agentRepository.registerNewAgent('user_call_sign');
+    //   // verify(() => mockAuthRepository.registerUser('user_call_sign'));
+    // });
+    //
+    // test('registerUser throws Exception on failure', () async {
+    //   when(() => mockAuthRepository.registerNewAgent(any()))
+    //       .thenThrow(Exception('Failed to register user'));
+    //
+    //   expect(
+    //     () async => await agentRepository.registerNewAgent('user_call_sign'),
+    //     throwsA(isA<Exception>()),
+    //   );
+    // });
   });
 }
